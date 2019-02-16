67eb246567177afd834438232e974c907769806f
yeyurusheng <wei17331350185@163.com>
2019-02-16 16:45:10 +0800

    debug

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -23,7 +23,7 @@ class WeixinController extends Controller{
     /** 接收微信服务器事件推送 */
     public function wxEvent(){
         $data=file_get_contents("php://input");
-        $log_str=date('Y-m-d H:i:s')."\n"."\n<<<<<<";
+        $log_str=date('Y-m-d H:i:s')."\n".$data."\n<<<<<<";
         file_put_contents('logs/wx_event.log',$log_str,FILE_APPEND);
     }
 }
