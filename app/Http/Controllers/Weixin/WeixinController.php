f65efb1885de6ad5b9f299128a6d04e959beb77b
yeyurusheng <wei17331350185@163.com>
2019-02-18 15:56:40 +0800

    debug

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -45,7 +45,7 @@ class WeixinController extends Controller{
             $user_info = $this->getUserInfo($openid);
             echo '<pre>';
             print_r($user_info);
-            echo '</pre>';
+            echo '</pre>';exit;
             //保存用户信息
             $u = WeixinUser::where(['openid' => $openid])->first();
             if ($u) {
@@ -91,10 +91,10 @@ class WeixinController extends Controller{
      */
     public function getUserInfo($openid){
         //$openid='wxc28bbb4fa8c63d2b';
-        $access_token=$this->getWXAccessToken();
-        $url='https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'openid='.$openid.'&lang=zh_CN';
-        $data=json_decode(file_get_contents($url),true);
+        $access_token = $this->getWXAccessToken();
+        $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
+        $data = json_decode(file_get_contents($url),true);
         //echo '<pre>';print_r($data);echo'</pre>';
-        //return $data;
+        return $data;
     }
 }
