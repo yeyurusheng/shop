52b2f3a4f1697cde3a1670ef22164bc5803c3c46
yeyurusheng <wei17331350185@163.com>
2019-02-18 15:42:11 +0800

    debug

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -90,10 +90,11 @@ class WeixinController extends Controller{
      * 获取用户信息
      */
     public function getUserInfo($openid){
-        $openid='wxc28bbb4fa8c63d2b';
+        //$openid='wxc28bbb4fa8c63d2b';
         $access_token=$this->getWXAccessToken();
         $url='https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'openid='.$openid.'&lang=zh_CN';
         $data=json_decode(file_get_contents($url),true);
-        echo '<pre>';print_r($data);echo'</pre>';
+        //echo '<pre>';print_r($data);echo'</pre>';
+        return $data;
     }
 }
