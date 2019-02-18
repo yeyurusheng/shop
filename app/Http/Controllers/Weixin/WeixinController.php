1ed0ff7c3975a2b948adea958390660f711bb705
yeyurusheng <wei17331350185@163.com>
2019-02-18 09:36:48 +0800

    获取用户信息

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -26,4 +26,28 @@ class WeixinController extends Controller{
         $log_str=date('Y-m-d H:i:s')."\n".$data."\n<<<<<<";
         file_put_contents('logs/wx_event.log',$log_str,FILE_APPEND);
     }
+    /** 获取微信access_token */
+    public function getWXAccessToken(){
+        //获取缓存
+        $token=Redis::get($this->redis_weixin_access_token);
+        if($token){
+            $url='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WEIXIN_APPID').'&secret='.env('WEIXIN_APPSECRET');
+            $data=json_decode(file_get_contents($url),true);
+            //记录缓存
+            $token=$data['access_token'];
+            Redis::set($this->redis_weixin_access_token,$token);
+            Redis::setTimeout($this->redis_weixin_access_token,3600);
+        }
+        return $token;
+    }
+    /**
+     * 获取用户信息
+     */
+    public function getUserInfo($openid){
+        $openid='wxc28bbb4fa8c63d2b';
+        $access_token=$this->getWXAccessToken();
+        $url='https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'openid='.$openid.'&lang=zh_CN';
+        $data=json_decode(file_get_contents($url),true);
+        echo '<pre>';print_r($data);echo'</pre>';
+    }
 }
